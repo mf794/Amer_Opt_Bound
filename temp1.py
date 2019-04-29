e5aafc33e8342c90507663d4702f79ddd68bd82e
You Xie <xyshell@bu.edu>
2019-04-29 16:35:30 -0400

    v1.0

--- temp1.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-bound = pd.read_csv('bound.csv', index_col=False, header=None)
-bound = pd.read_csv("bound.csv")
-z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
-z_data = z_data.iloc[:,1:]
